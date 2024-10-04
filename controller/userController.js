ecf23c159a8acf69a7cbe350be80a2356aa80dcb
Monica <167880958+monicanaidu@users.noreply.github.com>
2024-10-04 17:17:59 +0530

    crud-api

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,13 @@
 const { StatusCodes } = require("http-status-codes");
+const User = require("../model/userModel");
 
 let userController = {
   readAll: async (req, res) => {
     try {
-      res.json({ res: "read all" });
+      let data = await User.find({});
+      res
+        .status(StatusCodes.ACCEPTED)
+        .json({ status: true, length: data.length, users: data });
     } catch {
       res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
@@ -12,7 +16,10 @@ let userController = {
   },
   readSingle: async (req, res) => {
     try {
-      res.json({ res: "read single" });
+      let id = req.params.id;
+
+      let single = await User.findById(id);
+      res.status(StatusCodes.OK).json({ user: single });
     } catch {
       res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
@@ -21,16 +28,51 @@ let userController = {
   },
   createUser: async (req, res) => {
     try {
-      res.json({ res: "create user" });
+      let { name, email, mobile, age } = req.body;
+
+      //check email and mobile is already exists or not
+      let extEmail = await User.findOne({ email });
+      if (extEmail)
+        res
+          .status(StatusCodes.CONFLICT)
+          .json({ status: false, msg: `${email} already exists` });
+
+      let extMobile = await User.findOne({ mobile });
+      if (extMobile)
+        res
+          .status(StatusCodes.CONFLICT)
+          .json({ status: false, msg: `${mobile} already exists` });
+
+      //store the value in database
+      let newUser = await User.create({ name, email, mobile, age });
+
+      res.status(StatusCodes.CREATED).json({
+        status: true,
+        msg: " user info created successfully",
+        user: newUser,
+      });
     } catch {
       res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
         .json({ status: false, msg: err });
     }
   },
+
   updateUser: async (req, res) => {
     try {
-      res.json({ res: "update user" });
+      let id = req.params.id;
+
+      let extUser = await User.findById(id);
+      if (!extUser) {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ status: false, msg: `requested user id not found` });
+      }
+      //updatw method
+      await User.findByIdAndUpdate({ _id: id }, req.body);
+      res
+        .status(StatusCodes.ACCEPTED)
+        .json({ status: true, msg: `user info updated successfully` });
     } catch {
       res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
